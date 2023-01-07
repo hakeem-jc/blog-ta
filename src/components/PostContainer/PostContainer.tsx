eafc63b07c892ed45f1362fa9b6f58cd380197a5
hakeem-jc <hclarke.dev@gmail.com>
2023-01-07 10:34:49 -0500

    Add header to post list

--- src/components/PostContainer/PostContainer.tsx
+++ src/components/PostContainer/PostContainer.tsx
@@ -2,13 +2,26 @@ import { FC } from "react";
 import './PostContainer.css';
 import PostCarousel from "../PostCarousel/PostCarousel";
 import PostSummary from "../PostSummary/PostSummary";
+import Button from "../Button/Button";
 
 const PostContainer:FC = () => {
     return (
         <section className="post-container">
            <PostCarousel/>
 
+           <div className="post-container__header">
+             <h2 className="post-container__title">Recent Posts</h2>
+
+             <Button
+                    text={"Sort ASC"}
+                    type={"button"}
+                    shape="square"
+             />
+            </div>
+
            <div className="post-container__list">
+              
+
                 <PostSummary />
                 <PostSummary />
            </div>
