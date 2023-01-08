f889c16a8a6e5ad9d82eb9c8f4854264bbc263f8
hakeem-jc <hclarke.dev@gmail.com>
2023-01-08 12:51:58 -0500

    Setup testing

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from '../common/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const providerWrapper = (
+  <Provider store={store}>
+     <App />
+  </Provider>
+);
+
+test('smoke test', () => {
+  render(providerWrapper);
+  const app = screen.getByText(/blogr/i);
+  expect(app).toBeInTheDocument();
 });
